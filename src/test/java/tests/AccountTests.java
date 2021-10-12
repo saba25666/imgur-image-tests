bf4820fbf48edd2c1f2af659fcd640124f6ea453
saba25666 <Anzhelika1991>
2021-10-12 23:59:04 +0300

    Home Work 3

---
+++ src/test/java/tests/AccountTests.java
@@ -0,0 +1,81 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class AccountTests extends BaseTest{
+    @Test
+    void getAccountInfoTest() {
+        given()
+                .header("Authorization", token)
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .then()
+                .statusCode(200);
+    }
+    @Test
+    void getAccountInfoWithLoggingTest() {
+        given()
+                .header("Authorization", "Bearer 81ed217eee6d991be324edc8754a07e4ce686bb9")
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getAccountInfoWithAssertionsInGivenTest() {
+        given()
+                .header("Authorization", "Bearer 81ed217eee6d991be324edc8754a07e4ce686bb9")
+                .log()
+                .method()
+                .log()
+                .uri()
+                .expect()
+                .statusCode(200)
+                .body("data.url", equalTo(username))
+                .body("success", equalTo(true))
+                .body("status", equalTo(200))
+                .contentType("application/json")
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek();
+
+    }
+
+    @Test
+    void getAccountInfoWithAssertionsAfterTest() {
+        Response response = given()
+                .header("Authorization", "Bearer 81ed217eee6d991be324edc8754a07e4ce686bb9")
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek();
+        assertThat(response.jsonPath().get("data.url"), equalTo(username));
+    }
+
+    private static void getProperties(){
+        try (InputStream output = new FileInputStream("src/test/resources/application.properties")) {
+            properties.load(output);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
